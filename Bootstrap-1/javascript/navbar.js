e7b6160de5e68080d2cfd558895ea86fe49544e5
Md. Shobuj Shekh <mdshobuj968@gmail.com>
2024-05-31 23:05:02 +0600

    Add files via upload

---
+++ Bootstrap-1/javascript/navbar.js
@@ -0,0 +1,21 @@
+// var navList = document.getElementById("list-button");
+// var navBtns = navList.getElementsByClassName("nav-item");
+
+// for (var i = 0; i < navBtns.length; i++){
+//     navBtns.addEventListener("click", function() {
+//         var present = document.getElementsByClassName("active");
+//         present[0].className = present[0].className.replace("active", "");
+//         this.className += " active";
+//     });
+// }
+
+
+// var navLists = document.querySelectorAll('.nav-link');
+
+// navLists.forEach(navLists => {
+//     navLists.addEventListener('click', function(){
+//         document.querySelector('.acitve')?.classList.remove('active');
+//         this.classList.add('acitive');
+//     });
+// });
+
